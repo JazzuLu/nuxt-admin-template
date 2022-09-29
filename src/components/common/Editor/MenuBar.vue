4aa65e83941ee4231921bd87789184f75335df73
JazzuLu <358881374@qq.com>
2022-09-29 10:55:39 +0800

    feat: add few pages

---
+++ src/components/common/Editor/MenuBar.vue
@@ -0,0 +1,13 @@
+<template>
+$END$
+</template>
+
+<script>
+export default {
+name: "MenuBar"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/plugins/xxx.client.ts
@@ -0,0 +1,11 @@
+import { reactive } from 'vue'
+// import { defineNuxtPlugin } from '#imports'
+
+import CKEditor from '@ckeditor/ckeditor5-vue';
+
+
+export default defineNuxtPlugin(({ vueApp: app }) => {
+
+console.log('123')
+  // app.use(CKEditor);
+})
